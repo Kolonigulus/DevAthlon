93deae47140e5f007b443fe1b88cfb72f15ab8f2
Zortax <leonard.seibold.m@gmail.com>
2015-06-24 16:38:04 +0200

    Methoden
    onEnable() und onDisable() erstellt...

--- src/de/zortax/hier_kommt_der_plugin_name/PluginName.java
+++ src/de/zortax/hier_kommt_der_plugin_name/PluginName.java
@@ -4,4 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginName extends JavaPlugin {
 
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	
 }
