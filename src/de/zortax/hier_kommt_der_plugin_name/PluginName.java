00e66d55ab3f7098de17e298dcd4a40cd88bba99
Kolonigulus <kolonigulus@gmail.com>
2015-07-10 09:07:03 +0200

    SQLSetup und ListenerSetup hinzugefügt

--- src/de/zortax/hier_kommt_der_plugin_name/PluginName.java
+++ src/de/zortax/hier_kommt_der_plugin_name/PluginName.java
@@ -1,18 +1,63 @@
 package de.zortax.hier_kommt_der_plugin_name;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import mainSSQL.SQLTable;
+import mainSSQL.SSQLO;
+import mainSSQL.types.SQLType;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginName extends JavaPlugin {
+	SSQLO SQL;
+	String PluginName;
+	ArrayList<SQLTable> TableList;
+	CommandListener clisten;
+	MyEventListener elisten;
 
 	@Override
-	public void onEnable(){
-		System.out.println("Hallo Welt");
+	public void onEnable() {
+		try {
+			setUpSQL();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		registerEvents();
+
 	}
-	
+
 	@Override
-	public void onDisable(){
-		
+	public void onDisable() {
+
 	}
-	
-	
+
+	/**
+	 * @author Leonhard
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	private void setUpSQL() throws ClassNotFoundException, SQLException {
+		SQL = new SSQLO("localhost", "3306", PluginName, "testuser", "passwd");
+		HashMap<String, SQLType> header = new HashMap<String, SQLType>();
+		// TODO
+		TableList.add(SQL.CreateTable(header, "name"));
+		getLogger().info("MySQLVerbindung wurde hergestellt");
+	}
+
+	/**
+	 * @author Leonhard
+	 */
+	private void registerEvents() {
+		clisten = new CommandListener(this);
+		elisten = new MyEventListener(this);
+		this.getCommand(clisten.Command).setExecutor(clisten);
+		this.getServer().getPluginManager().registerEvents(elisten, this);
+		getLogger()
+				.info("CommandListener und EventListener wurden hinzugefügt");
+	}
+
 }
