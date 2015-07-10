a67d4e25b276db116929ad8b59c4c1cd756781cc
Zortax <leonard.seibold.m@gmail.com>
2015-07-10 15:47:52 +0200

    module und so

--- src/de/plababap/arcade/module/Module.java
+++ src/de/plababap/arcade/module/Module.java
@@ -1,7 +1,11 @@
 package de.plababap.arcade.module;
 
-public class Module {
+import org.bukkit.event.Listener;
 
+public class Module implements Listener {
+
+	// Autor: Leo (Zortax)
+	
 	// Dies ist die Superclass, die die einzelnen Minispiele dann erweitern
 	
 	private String name;
@@ -10,7 +14,24 @@ public class Module {
 	
 	public Module(String name){
 		
+		this.name = name;
+		this.ingame = false;
 		
+		
+		this.setup();
+				
+	}
+	
+	public final String getName(){
+		return name;
+	}
+	
+	
+	
+	
+	
+	public void setup(){
+		// Kann später vom entpsrechenden Module überschrieben werden, falls Minispielspezifisches irgendetwas passieren muss
 		
 	}
 	
