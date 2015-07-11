0f28caaf982defeef1acdd9395a9dee356407cf7
Kolonigulus <kolonigulus@gmail.com>
2015-07-11 08:19:55 +0200

    erste AplhaAlphaAlpa vom OITC Plugin

--- src/de/plababap/arcade/module/oitc/IngameOITCListener.java
+++ src/de/plababap/arcade/module/oitc/IngameOITCListener.java
@@ -1,10 +1,14 @@
 package de.plababap.arcade.module.oitc;
 
 import org.bukkit.entity.Damageable;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageModifier;
 
 /**
  * @author Leonhard
@@ -28,14 +32,24 @@ public class IngameOITCListener implements Listener {
 				}
 			}
 		}
-		
+
 	}
-	
+
 	@EventHandler
-	public void onCustomDeath(PlayerKilledEvent event){
-		oitc.deaths.replace((Player) event.getCause().getEntity(), oitc.deaths.get(event.getCause().getEntity()) + 1);
-		if(event.killedByPlayer()){
-			oitc.kills.replace(event.getKiller(), oitc.kills.get(event.getKiller()) + 1);
+	public void onCustomDeath(PlayerKilledEvent event) {
+		oitc.deaths.replace((Player) event.getCause().getEntity(),
+				oitc.deaths.get(event.getCause().getEntity()) + 1);
+		if (event.killedByPlayer()) {
+			oitc.kills.replace(event.getKiller(),
+					oitc.kills.get(event.getKiller()) + 1);
+		}
+	}
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onArrow(EntityDamageByEntityEvent event){
+		if(oitc.isIngame()){
+			if(event.getDamager().getType().equals(EntityType.ARROW)){
+				event.setDamage(50);
+			}
 		}
 	}
 
